77a345585dc989e9002461af38388e9019e27617
VivekBuzruk <vivek_buzruk@yahoo.com>
2021-04-08 19:44:07 +0530

    To Angular 11+

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {ErrorHandler, NgModule} from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
 
 // 3rd Party imports
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 // App imports
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {SharedModule} from './shared/shared.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
-  NbThemeModule,
-  NbLayoutModule,
   NbActionsModule,
-  NbUserModule,
+  NbIconModule,
+  NbLayoutModule,
   NbSearchModule,
-  NbIconModule
-} from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-import {NgxUIModule} from '@swimlane/ngx-ui';
-import {GlobalErrorHandler} from './app.error.handler';
+  NbThemeModule,
+  NbUserModule,
+} from "@nebular/theme";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import { NgxUIModule } from "@swimlane/ngx-ui";
+import { GlobalErrorHandler } from "./app.error.handler";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -33,18 +31,17 @@ import {GlobalErrorHandler} from './app.error.handler';
     CoreModule,
     NgbModule,
     SharedModule,
-    NbThemeModule.forRoot({name: 'dark'}),
+    NbThemeModule.forRoot({ name: "dark" }),
     NbLayoutModule,
     NbEvaIconsModule,
     NbActionsModule,
     NbUserModule,
     NgxUIModule,
     NbSearchModule,
-    NbIconModule
+    NbIconModule,
   ],
   providers: [{ provide: ErrorHandler, useClass: GlobalErrorHandler }],
   bootstrap: [AppComponent],
-  exports: []
+  exports: [],
 })
-export class AppModule {
-}
+export class AppModule {}
